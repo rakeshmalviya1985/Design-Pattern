89483ef3921a55ef0a9f64e4abe8050abff0c150
rakeshmalviya1985 <31981319+rakeshmalviya1985@users.noreply.github.com>
2017-09-17 11:28:51 +0530

    Create Structural_adapterDesignPattern.cpp

---
+++ Structural_adapterDesignPattern.cpp
@@ -0,0 +1,6 @@
+/*
+The Adapter Design pattern allows  incompatible classes to work together by converting the interface of one class into another
+
+Example 1:
+Consider , my socket(flat pin) is as per the Europe standard however my electric kettle have  US based pin(Round) so both are different all to gather however we want to make things  work , in that case adapter will come into the picture so we have to create one adapter which will help  both the interface to work together.
+*/
